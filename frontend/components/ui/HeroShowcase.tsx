f9fbe507d93e28b64fc52866f8ce9aef65bce9b3
d.larin <larin.dn@icloud.com>
2026-01-29 10:50:16 +0300

    f2.7.2

--- frontend/components/ui/HeroShowcase.tsx
+++ frontend/components/ui/HeroShowcase.tsx
@@ -18,7 +18,7 @@ export function HeroShowcase() {
           className="
             absolute
             bottom-[-10px]
-            left-[70%]
+            left-[65%]
             -translate-x-1/2
             z-0
           "
