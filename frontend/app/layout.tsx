95cb709b3c45a4f554856d77bf4ac1d889dcbcbb
d.larin <larin.dn@icloud.com>
2026-02-22 19:19:11 +0300

    f6.2.4

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const manrope = Roboto({
+const manrope = Manrope({
   subsets: ["latin", "cyrillic"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-sans",
