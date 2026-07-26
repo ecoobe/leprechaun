b4ccebc0fa1b30a31a3f75f70a7512b40be6fe5e
d.larin <larin.dn@icloud.com>
2026-07-26 19:07:44 +0400

    b.17

--- frontend/components/ui/Logo.tsx
+++ frontend/components/ui/Logo.tsx
@@ -8,19 +8,19 @@ export function Logo() {
       href="/"
       aria-label="coobe"
       className="
-  absolute
-  top-6
-  left-10
-  z-20
-  select-none
-  text-[34px]
-  font-medium
-  tracking-[-0.05em]
-  text-zinc-100
-  transition-colors
-  duration-200
-  hover:text-white
-"
+        absolute
+        top-6
+        left-10
+        z-20
+        select-none
+        text-3xl
+        font-regular
+        tracking-[-0.04em]
+        text-zinc-100
+        transition-colors
+        duration-200
+        hover:text-white
+      "
     >
       coobe
     </Link>
